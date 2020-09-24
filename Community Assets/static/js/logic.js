ed22091f47f4b38b6b541c85a55aef1c43eff1f3
McGlash <64424813+McGlash@users.noreply.github.com>
2020-09-24 18:30:53 -0400

    intergration complete; need to further integrate events with pop-up closure

--- Community Assets/static/js/logic.js
+++ Community Assets/static/js/logic.js
@@ -1,4 +1,4 @@
-function createMap (holder1, holder2, holder3, holder4, holder5, holder6, holder7, rental) {
+function createMap (assetArray, rental, crime, data) {
 
   //create fsa layer from FSA geoJSON file
 
@@ -18,16 +18,21 @@ function createMap (holder1, holder2, holder3, holder4, holder5, holder6, holder
 
     // Create an overlayMaps object to hold the community asset layer
   var overlayMaps = {
-      FSA: FSA,
-      "community Services" : holder1,
-      "Education & Employment": holder2,
-      "Financial Services" : holder3,
-      "Food & Housing" : holder4,
-      "Health Services" : holder5,
-      "Law & Government": holder6,
-      "Tranportation" : holder7, 
-      "Rental Postings" : rental
+      "Community Services" : assetArray[0],
+      "Education & Employment": assetArray[1],
+      "Financial Services" : assetArray[2],
+      "Food & Housing" : assetArray[3],
+      "Health Services" : assetArray[4],
+      "Law & Government": assetArray[5],
+      "Tranportation" : assetArray[6]
     };
+
+    // Define a baseMaps object to hold our base layers
+    var baseMaps = {
+          "Rental Postings" : rental,
+          "2019 Homicides" : crime
+      };
+  
 
   // create tile layer
   var streetview = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -47,7 +52,13 @@ function createMap (holder1, holder2, holder3, holder4, holder5, holder6, holder
   });
 
   // Pass map layers into layer control and add the layer control to the map
-  L.control.layers(null, overlayMaps, {collapsed:false}).addTo(TorontoMap);
+  L.control.layers(baseMaps, overlayMaps, {collapsed:false}).addTo(TorontoMap);
+
+  mapLegend(TorontoMap)
+
+  RentalCrimeInteraction(rental, data, TorontoMap)
+
+  return TorontoMap
 };
 
 function ReadLayersDisplay () {
@@ -98,22 +109,61 @@ function ReadLayersDisplay () {
         }
     });
 
-          //create rental and crime postings
-          d3.csv('static/data/Rental_Craigslist.csv', function(rental){
+    var assetArray = [servicesAsset, educationAsset, 
+                      financialAsset, foodAsset, 
+                      healthAsset, lawAsset, transportAsset]
 
-              //rental posting layer
+          //read rental posting dataset
+          d3.csv('static/js/Rental_Craigslist.csv', function(rental){
+
+              //create rental posting layer
               var rentalMarkers = [];
 
               rental.forEach(function(feature) {
-                rentalMarkers.push(L.marker([feature.lat, feature.long]).bindPopup(feature.title));
+
+                var rental = L.ExtraMarkers.icon({
+                  markerColor: "green-light",
+                  svg: true,
+                  number: rentalText(feature.bedrooms),
+                  shape: 'circle',
+                  iconColor: "white",
+                  icon: 'fa-number'
+                });
+
+                rentalMarkers.push(L.marker([feature.lat, feature.long], {
+                  icon: rental
+                }).bindPopup(feature.title));
               });
 
               var rentalMarkerGroup = L.layerGroup(rentalMarkers);
+              
+              var CrimeMarkers = [];
 
-            createMap(servicesAsset, educationAsset, 
-              financialAsset, foodAsset, 
-              healthAsset, lawAsset, transportAsset, rentalMarkerGroup);
-          
+              //read crime dataset
+              d3.csv('static/js/Crime.csv', function(fullcrime){
+
+                var crimeIcon = L.ExtraMarkers.icon({
+                  markerColor: "blue",
+                  icon: "fa-alert-outline",
+                  shape: 'penta',
+                  iconColor: "white",
+                  prefix: 'fa'
+                });
+
+                var murder = fullcrime.filter(d=>d.MCI=="Homicide");
+
+                murder.forEach(row => {
+                  CrimeMarkers.push(L.marker([row.lat, row.long],{
+                    icon: crimeIcon
+                  }).bindPopup(row.MCI));
+                
+                });
+                
+                var CrimeMarkerGroup = L.layerGroup(CrimeMarkers);
+
+                createMap(assetArray, rentalMarkerGroup, CrimeMarkerGroup, fullcrime);
+                
+              });
           });
     });
 
@@ -122,9 +172,40 @@ function ReadLayersDisplay () {
 
 ReadLayersDisplay ();
 
+//function for creating legend
+function mapLegend (map) {
 
+  colors = ["yellow", "red", "orange", "green", "purple", "blue", "#0079A4"];
 
-//function for styling polygons
+  var legend = L.control({position: 'bottomright'});
+
+  legend.onAdd = function () {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+                  categories = ["Community Services", 
+                                'Education & Employment',
+                                "Financial Services",
+                                "Food & Housing",
+                                "Health Services",
+                                "Law & Government",
+                                "Transportation"
+                              ],
+                  labels =[];
+    
+    div.innerHTML += '<strong> Community Asset </strong> <br>'
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < categories.length; i++) {
+        div.innerHTML +=
+            '<i class="circle" style="background:' + colors[i] + '"></i> ' +
+            categories[i] + '<br>';
+    };
+    return div;
+ };
+legend.addTo(map);
+
+};
+
+//function for styling FSA polygons
 function FSAStyle (feature) {
   return {
       fillColor: null,
@@ -150,7 +231,7 @@ function assestColors(type){
     case "Law & Government":
       return "blue";
     case "Transportation":
-        return "grey";
+        return "#0079A4";
     default:
       return "black";
     }
@@ -166,7 +247,7 @@ function markerAllocation(agencyName, category, lat, long, array){
       color: assestColors(category),
       fillColor: assestColors(category),
       fillOpacity: 0.4,
-      radius: 50
+      radius: 90
       });
 
     marker.bindTooltip(`<h2> ${agencyName}</h2>`);
@@ -175,4 +256,104 @@ function markerAllocation(agencyName, category, lat, long, array){
     array.addLayer(marker);
 
     return array
+};
+
+
+//function to determine text on rental icons
+
+function rentalText (text){
+  if(text > 0){
+    return parseInt(text)
+      }
+  else{
+    return "?"
+  };
+};
+
+//function to draw circle around rental posting and display crime incidences within it
+function RentalCrimeInteraction(rentalMarkerGroup, data, map){
+
+      var radius = 1000;
+      var activeCircle = false;
+
+      var r = 0; //in meters
+      var circleCenterPoint = [0,0]; //gets the circle's center latlng
+      var isInCircleRadius = false;
+
+      var intCrimeMarkerGroup = L.layerGroup([]);
+
+      //Draw circle when clicked
+      rentalMarkerGroup.getLayers().forEach(function(obj) {
+        
+        obj.on('click', function(e) {
+          // marker clicked is e.target
+       
+          // remove active circle if any
+          if(activeCircle) {
+            map.removeLayer(activeCircle);
+          }
+          // draw a 1km circle with the same center as the marker 
+          activeCircle = L.circle(e.target.getLatLng(), { 
+            radius: radius ,
+             fillColor: "grey",
+
+             }).addTo(map);
+
+          r = activeCircle.getRadius(); //in meters
+          circleCenterPoint = activeCircle.getLatLng(); //gets the circle's center latlng
+          if(intCrimeMarkerGroup) {
+            map.removeLayer(intCrimeMarkerGroup);
+          }
+
+          crimeMarkers = []
+
+          for (var i = 0; i < data.length; i++) {
+            isInCircleRadius = Math.abs(circleCenterPoint.distanceTo([data[i].lat, data[i].long])) <= r;
+            
+            var crimeIcon = L.ExtraMarkers.icon({
+              markerColor: crimeColors(data[i].MCI),
+              icon: "fa-alert-outline",
+              shape: 'penta',
+              iconColor: "white",
+              prefix: 'fa'
+            });
+
+            // add marker only if the point is within the area
+            if (isInCircleRadius) {
+              crimeMarkers.push(L.marker([data[i].lat, data[i].long], {
+                radius: 50,
+                icon: crimeIcon
+              }).bindPopup(data[i].MCI));
+
+            crimeMarkers.getPopup().on('remove', function() {
+                //Your code here
+            });
+            }
+          intcrimeMarkerGroup = L.layerGroup(crimeMarkers).addTo(map);
+          }
+
+        });
+    });
+  };
+
+  // function for styling community assets
+
+function crimeColors(type){
+  switch (type) {
+    case "Assault":
+      return "yellow";
+    case "Auto Theft":
+      return "red";
+    case "Break and Enter":
+      return "orange";
+    case "Homicide":
+      return "blue";
+    case "Robbery":
+      return "purple";
+    case "Theft Over":
+        return "#0079A4";
+    default:
+      return "black";
+    }
+  
 };
